7fc6359bb1b6b843d9c3723dd8f22831bc20b842
Anubhav990 <anubhavg2k3@gmail.com>
2024-08-14 14:37:26 +0530

    final changes made

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -3,7 +3,7 @@ import { faqCards } from '../data/faqcards';
 
 function FAQ({ iconFaq = faqIcon }) {
     return (
-        <div className="bg-white py-6 sm:py-8 lg:py-12">
+        <div className="bg-white py-6 sm:py-8 lg:py-12 hidden md:block">
             <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
                 {/* text - start */}
                 <div className="mb-10 md:mb-20">
