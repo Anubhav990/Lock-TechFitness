f0c7225e5747346dbfb293336ef7287888ab273d
Anubhav990 <anubhavg2k3@gmail.com>
2024-08-14 14:42:18 +0530

    final changes made

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -100,7 +100,7 @@ function Content({
     return (
         <section className="text-gray-900 md:text-gray-600 body-font bg-white md:bg-gradient-to-b md:from-white md:via-blue-50 md:to-blue-100 rounded-lg">
             <div className="container px-5 md:pt-28 pt-16 pb-4 md:pb-16 mx-auto">
-                <div className="flex flex-wrap w-full mb-14 md:mb-20 flex-col items-center text-center">
+                <div className="flex flex-wrap w-full mb-6 md:mb-20 flex-col items-center text-center">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-6 md:mb-2 text-gray-900">Transform Your Body and Mind with Advanced Fitness Insights and Tech</h1>
                     <p className="lg:w-3/4 w-full leading-relaxed text-gray-500 font-medium">Discover expert fitness advice, advanced hypertrophy tips and how to use technology to enhance your health journey.</p>
                 </div>
